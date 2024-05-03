cf83ed41a1dffb137339203a96a2bb9a1470fc75
VOLKKODAV <167250805+VOLKKODAV@users.noreply.github.com>
2024-05-04 01:46:19 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list = ['blanket', 'ticket', 'flight', 'appointment', 'bag', 'hotel']
+print(my_list)
+print(my_list[0::5])
+print(my_list[2:5])
+my_list[2] = 'room'
+print(my_list)
+
+my_dict = {'blanket': 'одеяло', 'ticket': 'билет', 'flight': 'рейс'}
+print(my_dict)
+print(my_dict['ticket'])
+my_dict['blanket'] = 'שמיכה'
+my_dict['room'] = 'комната'
+print(my_dict)
